3d541b58e4145a7024778ff58da2698282a6bdd5
udesc <udes@udesc>
2015-10-02 06:29:22 -0300

    alterações feitas na aula

--- Matriz/Matriz.cpp
+++ Matriz/Matriz.cpp
@@ -11,7 +11,7 @@
 
 
 using namespace std;
-
+/*
 template<class E,int rows,int cols>
 Matriz<E, rows, cols>::Matriz(E valor){
 	for(int i = 0 ; i < rows ; i++){
@@ -19,22 +19,22 @@ Matriz<E, rows, cols>::Matriz(E valor){
 			mat[i][j] = valor;
 		}
 	}
-}
+}*/
 
 /*template<class E,int rows,int cols>
 Matriz<E, rows, cols>::~Matriz(){
 	delete[E] mat[rows][cols];
 }*/
-
+/*
 template<class E,int rows,int cols>
 void Matriz<E, rows, cols>::setValue(int row,int col, E value){
 	this->mat[row][col] = value;
-}
-
+}*/
+/*
 template<class E,int rows,int cols>
 E Matriz<E, rows, cols>::at(int row,int col){
 	return this->mat[row][col];
-}
+}*/
 /*
 template<class E,int rows,int cols>
 Matriz<E,rows,cols> Matriz<E, rows, cols>::operator+(Matriz<E,rows,cols> b){
@@ -70,7 +70,7 @@ Matriz<E,rows,cols> Matriz<E,rows,cols>::operator-(){
 	return retMat;
 }*/
 
-template<class E,int rows,int cols>
+/*template<class E,int rows,int cols>
 void Matriz<E, rows, cols>::printMatriz(){
 	for(int i = 0 ; i < rows ; i ++){
 		for(int j = 0 ; j < cols ; j ++){
@@ -80,7 +80,7 @@ void Matriz<E, rows, cols>::printMatriz(){
 	}
 	cout<<endl;
 }
-
+*/
 
 
 
@@ -126,7 +126,7 @@ int main(){
 
 	//(a+b).printMatriz();
 	//(b-a).printMatriz();
-	(a*b).printMatriz();
+	(a^2).printMatriz();
 
 	//(-a).printMatriz();
 	//system("pause");
--- Matriz/Matriz.h
+++ Matriz/Matriz.h
@@ -8,28 +8,70 @@
 #ifndef MATRIZ_H_
 #define MATRIZ_H_
 
+using namespace std;
+
 template<class E, int rows, int cols>
 class Matriz{
     private:
         E mat[rows][cols];
     public:
-        Matriz(E valor);
-        //~Matriz();
-        void setValue(int row,int col, E value);
-        E at(int row, int col);
-        //Matriz<E,rows,cols> operator+(Matriz<E,rows,cols> b);
-        //Matriz<E,rows,cols> operator-(Matriz<E,rows,cols> b);
-        //Matriz<E,rows,cols> operator-();
-        template<E, int r, int c>
+        Matriz(E valor){
+            for(int i = 0 ; i < rows ; i++){
+                for(int j = 0 ; j < cols ; j++){
+                    mat[i][j] = valor;
+                }
+            }
+        }
 
-            template<int r,int c> 
+        //~Matriz();
+        void setValue(int row,int col, E value){
+            this->mat[row][col] = value;
+        }
+
+        E at(int row, int col){
+            return this->mat[row][col];
+        }
+
+        Matriz<E,rows,cols> operator+(Matriz<E,rows,cols> b){
+            Matriz<E,rows,cols> retMat(0);
+            for(int i = 0 ; i < rows ; i ++){
+                for(int j = 0 ; j < cols ; j ++) {
+                    retMat.setValue(i,j, at(i,j)+b.at(i,j));
+                }
+            }
+            return retMat;
+        }
+        Matriz<E,rows,cols> operator-(Matriz<E,rows,cols> b){  
+            Matriz<E,rows,cols> retMat(0);
+            for(int i = 0 ; i < rows ; i ++){
+                for(int j = 0 ; j < cols ; j ++) {
+                    retMat.setValue(i,j, at(i,j)-b.at(i,j));
+                }
+            }
+            return retMat;
+        }
+
+        Matriz<E,rows,cols> operator-(){
+            Matriz<E,rows,cols> retMat(0);
+            for(int i = 0 ; i < rows ; i++){
+                for(int j = 0 ; j < cols ; j++){
+                    retMat.setValue(i,j,-at(i,j));
+                }
+            }
+            return retMat;
+        }
+
+
+        template<int r,int c> 
             Matriz<E,cols,r> operator*(const Matriz<E,r,c> &b){
+                if(verifica(rows,c)){
+                
                 Matriz<E,cols,r> retMat(0);
                 E sum = 0;
 
                 for(int k = 0 ; k < rows ; k++){
-                    for(int i = 0; i < cols ; i ++){
-                        for(int j = 0 ; j < cols ; j ++) {
+                    for(int i = 0; i < cols; i ++){
+                        for(int j = 0 ; j < r; j ++) {
                             sum+=(at(i,j)*b.at(j,i));
                         }
                         retMat.setValue(k,i,sum);
@@ -37,10 +79,42 @@ class Matriz{
                     }
                 }
                 return retMat;
+                }else{
+                    cout<<"Matrizes inválidas,  número de linhas diferente do número de colunas!"<<endl;
+                }
             }
-        // rator*(const Matriz &b);
-        //Matriz<E,row,cols>  operator^(const E &b);
-        void printMatriz();
+
+        Matriz<E,rows,cols>  operator^(const E &b){
+            Matriz<E,rows,cols> retMat(0);
+            E sum = 0;
+
+            for(int k = 0 ; k < rows ; k++){
+                for(int i = 0; i < cols ; i ++){
+                    for(int j = 0 ; j < cols ; j ++) {
+                        sum+=(at(i,j)*retMat.at(j,i));
+                    }
+                    retMat.setValue(k,i,sum);
+                    sum=0;
+                }
+            }
+
+            return retMat;
+        }
+
+        void printMatriz(){
+            for(int i = 0 ; i < rows ; i ++){
+                for(int j = 0 ; j < cols ; j ++){
+                    cout<<mat[i][j]<<" ";
+                }
+                cout<<endl;
+            }
+            cout<<endl;
+        }
+
+        bool verifica(int c1, int r2){
+            if(c1==r2)return true;
+            else return false;
+        }
 };
 
 #endif /* MATRIZ_H_ */
